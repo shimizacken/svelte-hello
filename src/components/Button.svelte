bb4ad0886c9f721923aae45f80d351c8201b581a
shimizacken <shimizacken@gmail.com>
2020-09-06 16:40:16 +0200

    typescript support

---
+++ src/components/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import { ButtonType } from "../types/buttonTypes";
+
+
+    export let onClick: (e: MouseEvent) => void;
+    export let text: string;
+    export let buttonType: ButtonType;
+</script>
+
+<button on:click={onClick} type={buttonType}>{text}</button>
---
+++ src/components/Logo.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Img from './Img.svelte';
+</script>
+
+<Img src='https://weather-fetcher.netlify.app/images/37da637a7284a31fc5d6196b198da0ad.png' alt="Logo" />
